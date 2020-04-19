a5d073187d780d2675dfe7127c104ce25542c624
Wesley Costa <wesley_costa@outlook.com>
2020-04-18 22:04:49 -0300

    Correção

--- service.cs
+++ service.cs
@@ -44,7 +44,7 @@ namespace RabbitMQ.Services
             }
         }
 
-        private void Customer()
+        private void Consumer()
         {
             var factory = new ConnectionFactory()
             {
